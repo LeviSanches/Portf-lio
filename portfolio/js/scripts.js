ba6d188fc44f7d22e23b0d8fe84a736d9424550d
Levi Sanches <110796789+LeviSanches@users.noreply.github.com>
2023-06-25 17:15:37 -0300

    Delete portfolio directory

--- portfolio/js/scripts.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    const offcanvas = document.querySelector('.offcanvas');
-    const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
-    offcanvasInstance.hide();
-  });
-});
